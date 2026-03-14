de4d0dd8bec2fe8e660d9c7e799cd692e1d3e151
sakthi-28 <sakhtivelvenkat2619@gmail.com>
2026-03-14 12:00:31 +0530

    added icons in the public folder and changes in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,35 @@ import "./globals.css"
 export const metadata: Metadata = {
   title: "Sakthivel V — Portfolio",
   description: "Portfolio of Sakthivel V",
-    generator: 'v0.app'
+  generator: "v0.app",
+
+  icons: {
+    icon: [
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/icons/android-icon-192x192.png", sizes: "192x192", type: "image/png" }
+    ],
+    apple: [
+      { url: "/icons/apple-icon-57x57.png", sizes: "57x57" },
+      { url: "/icons/apple-icon-60x60.png", sizes: "60x60" },
+      { url: "/icons/apple-icon-72x72.png", sizes: "72x72" },
+      { url: "/icons/apple-icon-76x76.png", sizes: "76x76" },
+      { url: "/icons/apple-icon-114x114.png", sizes: "114x114" },
+      { url: "/icons/apple-icon-120x120.png", sizes: "120x120" },
+      { url: "/icons/apple-icon-144x144.png", sizes: "144x144" },
+      { url: "/icons/apple-icon-152x152.png", sizes: "152x152" },
+      { url: "/icons/apple-icon-180x180.png", sizes: "180x180" }
+    ],
+  },
+
+  manifest: "/manifest.json",
+
+  
+}
+
+export const viewport = {
+  themeColor: "#ffffff",
 }
 
 const spaceGrotesk = Space_Grotesk({
@@ -28,9 +56,18 @@ const dmSans = DM_Sans({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning className={`${spaceGrotesk.variable} ${dmSans.variable} antialiased`}>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${spaceGrotesk.variable} ${dmSans.variable} antialiased`}
+    >
       <body className="font-sans bg-background text-foreground">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <SiteHeader />
           {children}
           <SiteFooter />
